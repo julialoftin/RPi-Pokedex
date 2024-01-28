8d7ebcd1dddc35ec841559b97153cb6e980c92be
Julia Loftin <julia.loftin7@gmail.com>
2024-01-28 14:44:29 -0600

    creates new pokedex file

---
+++ pokedex.py
@@ -0,0 +1,70 @@
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+import requests
+
+# Creates the i2c interface
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Creates the SSD1306 OLED class
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+# Input pins:
+button_A = DigitalInOut(board.D5)
+button_A.direction = Direction.INPUT
+button_A.pull = Pull.UP
+
+button_B = DigitalInOut(board.D6)
+button_B.direction = Direction.INPUT
+button_B.pull = Pull.UP
+
+button_U = DigitalInOut(board.D17)
+button_U.direction = Direction.INPUT
+button_U.pull = Pull.UP
+
+button_D = DigitalInOut(board.D22)
+button_D.direction = Direction.INPUT
+button_D.pull = Pull.UP
+
+# API URLs
+
+
+# Create an off-screen buffer and drawing object for Menu List
+buffer_main_menu = Image.new("1", (disp.width, disp.height))
+draw_main_menu = ImageDraw.Draw(buffer_main_menu)
+
+# Define states
+MAIN_MENU_STATE = 0
+
+# Initialize the current state and the selected menu item
+current_state = MAIN_MENU_STATE
+
+# Initialize start index for scrolling
+start_index_menu = 0
+
+def clear_buffer(buffer, draw):
+    draw.rectangle((0, 0, buffer.width, buffer.height), outline=0, fill=0)
+
+def update_display_main_menu(selected_index_main_menu):
+    clear_buffer(buffer_main_menu, draw_main_menu)
+    draw_main_menu.text((0, 0) "PokeDictionary", fill=1)
+
+    main_menu_items = ["Generations", "Items", "Pokemon"]
+
+    for i, item in enumerate(main_menu_items):
+        display_text = item
+
+        if i == selected_index_main_menu:
+            display_text = f"# {display_text}"
+
+        draw_main_menu.text(0, (i * 10), display_text, fill=1)
+
+    disp.image(buffer_main_menu)
+    disp.show()
+
+
+while True:
+    if current_state == MAIN_MENU_STATE:
+        try: 
