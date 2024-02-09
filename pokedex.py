307cf1f3fb5192a0362d4ab627ce337ae25a78f1
Julia Loftin <julia.loftin7@gmail.com>
2024-02-09 12:50:14 -0600

    corrects a button if statement

--- pokedex.py
+++ pokedex.py
@@ -452,7 +452,7 @@ while True:
                 update_display_generation_i_menu(selected_index_generation_i_menu)
             if not button_A.value:
                 print("Button A Pressed")
-                if selected_index_generation_i_menu = 0:
+                if selected_index_generation_i_menu == 0:
                     current_state = GENERATION_I_MAIN_REGION_MENU_STATE
                     break
             if not button_B.value:
