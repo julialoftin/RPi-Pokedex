7d2d1006ffddfee13afdd11fbb7b16442cdf0f47
Julia Loftin <julia.loftin7@gmail.com>
2024-02-09 14:27:49 -0600

    adds gen viii main region menu state

--- pokedex.py
+++ pokedex.py
@@ -85,6 +85,9 @@ draw_generation_vi_main_region_menu = ImageDraw.Draw(buffer_generation_vi_main_r
 # Create an off-screen buffer and drawing object for Generation VII Main Region Menu
 buffer_generation_vii_main_region_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_vii_main_region_menu = ImageDraw.Draw(buffer_generation_vii_main_region_menu)
+# Create an off-screen buffer and drawing object for Generation VIII Main Region Menu
+buffer_generation_viii_main_region_menu = Image.new("1", (disp.width, disp.height))
+draw_generation_viii_main_region_menu = ImageDraw.Draw(buffer_generation_viii_main_region_menu)
 
 # Define states
 MAIN_MENU_STATE = 0
@@ -106,6 +109,7 @@ GENERATION_IV_MAIN_REGION_MENU_STATE = 14
 GENERATION_V_MAIN_REGION_MENU_STATE = 15
 GENERATION_VI_MAIN_REGION_MENU_STATE = 16
 GENERATION_VII_MAIN_REGION_MENU_STATE = 17
+GENERATION_VIII_MAIN_REGION_MENU_STATE = 18
 
 # Initialize the current state and the selected menu item
 current_state = MAIN_MENU_STATE
@@ -130,6 +134,7 @@ start_index_generation_iv_main_region_menu = 0
 start_index_generation_v_main_region_menu = 0
 start_index_generation_vi_main_region_menu = 0
 start_index_generation_vii_main_region_menu = 0
+start_index_generation_viii_main_region_menu = 0
 
 def clear_buffer(buffer, draw):
     draw.rectangle((0, 0, buffer.width, buffer.height), outline=0, fill=0)
@@ -234,6 +239,17 @@ def fetch_generation_vii_data():
         if response.status_code == 200:
             generation_vii_data = response.json()
             return generation_vii_data
+        else:
+            print(f"Failed to get Generations data. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+
+def fetch_generation_viii_data():
+    try:
+        response = requests.get(generation_api_url + "/8/")
+        if response.status_code == 200:
+            generation_viii_data = response.json()
+            return generation_viii_data
         else:
             print(f"Failed to get Generations data. Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
@@ -597,6 +613,31 @@ def update_display_generation_vii_main_region_menu(selected_index_generation_vii
     disp.image(buffer_generation_vii_main_region_menu)
     disp.show()
 
+def update_display_generation_viii_main_region_menu(selected_index_generation_viii_main_region_menu):
+    clear_buffer(buffer_generation_viii_main_region_menu, draw_generation_viii_main_region_menu)
+    draw_generation_viii_main_region_menu.text((0, 0), "Main Regions:", fill=1)
+
+    display_count = 5
+    generation_viii_data = fetch_generation_viii_data()
+    main_region_data = [generation_viii_data["main_region"]]
+    total_generation_viii_main_region_menu_items = len(main_region_data)
+    start_index_generation_viii_main_region_menu = 0
+    max_visible_items = min(display_count, total_generation_viii_main_region_menu_items - start_index_generation_viii_main_region_menu)
+
+    for i in range(max_visible_items):
+        if start_index_generation_viii_main_region_menu + i < total_generation_viii_main_region_menu_items:
+            region_name = main_region_data[start_index_generation_viii_main_region_menu + i].get("name", "")
+
+            display_text = f"{region_name}"
+
+            if i + start_index_generation_viii_main_region_menu == selected_index_generation_viii_main_region_menu:
+                display_text = f"# {display_text}"
+
+            draw_generation_viii_main_region_menu.text((0, (i * 10) + 10), display_text, fill=1)
+
+    disp.image(buffer_generation_viii_main_region_menu)
+    disp.show()
+
 while True:
 
     if current_state == MAIN_MENU_STATE:
@@ -892,6 +933,11 @@ while True:
                 if selected_index_generation_viii_menu >= total_generation_viii_menu_items:
                     selected_index_generation_viii_menu = 0
                 update_display_generation_viii_menu(selected_index_generation_viii_menu)
+            if not button_A.value:
+                print("Button A Pressed")
+                if selected_index_generation_viii_menu == 0:
+                    current_state = GENERATION_VIII_MAIN_REGION_MENU_STATE
+                    break
             if not button_B.value:
                 print("Button B Pressed")
                 current_state = GENERATIONS_MENU_STATE
@@ -1093,4 +1139,29 @@ while True:
             if not button_B.value:
                 print("Button B Pressed")
                 current_state = GENERATION_VII_MENU_STATE
+                break
+
+    if current_state == GENERATION_VIII_MAIN_REGION_MENU_STATE:
+        selected_index_generation_viii_main_region_menu = 0
+        generation_viii_data = fetch_generation_viii_data()
+        main_region_data = [generation_viii_data["main_region"]]
+        total_generation_viii_main_region_menu_items = len(main_region_data)
+        update_display_generation_viii_main_region_menu(selected_index_generation_viii_main_region_menu)
+
+        while True:
+            if not button_U.value:
+                print("Button U Pressed")
+                selected_index_generation_viii_main_region_menu = (selected_index_generation_viii_main_region_menu - 1) % total_generation_viii_main_region_menu_items
+                if selected_index_generation_viii_main_region_menu < 0:
+                    selected_index_generation_viii_main_region_menu = total_generation_viii_main_region_menu_items - 1
+                update_display_generation_viii_main_region_menu(selected_index_generation_viii_main_region_menu)
+            if not button_D.value:
+                print("Button D Pressed")
+                selected_index_generation_viii_main_region_menu = (selected_index_generation_viii_main_region_menu + 1) % total_generation_viii_main_region_menu_items
+                if selected_index_generation_viii_main_region_menu >= total_generation_viii_main_region_menu_items:
+                    selected_index_generation_viii_main_region_menu = 0
+                update_display_generation_viii_main_region_menu(selected_index_generation_viii_main_region_menu)
+            if not button_B.value:
+                print("Button B Pressed")
+                current_state = GENERATION_VIII_MENU_STATE
                 break
