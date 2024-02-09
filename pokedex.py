49fbcb13b710ebf0a667c24fe9cd2befba7a28f7
Julia Loftin <julia.loftin7@gmail.com>
2024-02-09 11:43:02 -0600

    begins adding gen i main region functionality

--- pokedex.py
+++ pokedex.py
@@ -31,39 +31,42 @@ button_D.pull = Pull.UP
 # API URLs
 generation_api_url = "https://pokeapi.co/api/v2/generation"
 
-# Create an off-screen buffer and drawing object for Main Menu List
+# Create an off-screen buffer and drawing object for Main Menu
 buffer_main_menu = Image.new("1", (disp.width, disp.height))
 draw_main_menu = ImageDraw.Draw(buffer_main_menu)
-# Create an off-screen buffer and drawing object for Generations Menu List
+# Create an off-screen buffer and drawing object for Generations Menu
 buffer_generations_menu = Image.new("1", (disp.width, disp.height))
 draw_generations_menu = ImageDraw.Draw(buffer_generations_menu)
-# Create an off-screen buffer and drawing object for Generation I Menu List
+# Create an off-screen buffer and drawing object for Generation I Menu
 buffer_generation_i_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_i_menu = ImageDraw.Draw(buffer_generation_i_menu)
-# Create an off-screen buffer and drawing object for Generation II Menu List
+# Create an off-screen buffer and drawing object for Generation II Menu
 buffer_generation_ii_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_ii_menu = ImageDraw.Draw(buffer_generation_ii_menu)
-# Create an off-screen buffer and drawing object for Generation III Menu List
+# Create an off-screen buffer and drawing object for Generation III Menu
 buffer_generation_iii_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_iii_menu = ImageDraw.Draw(buffer_generation_iii_menu)
-# Create an off-screen buffer and drawing object for Generation IV Menu List
+# Create an off-screen buffer and drawing object for Generation IV Menu
 buffer_generation_iv_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_iv_menu = ImageDraw.Draw(buffer_generation_iv_menu)
-# Create an off-screen buffer and drawing object for Generation V Menu List
+# Create an off-screen buffer and drawing object for Generation V Menu
 buffer_generation_v_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_v_menu = ImageDraw.Draw(buffer_generation_v_menu)
-# Create an off-screen buffer and drawing object for Generation VI Menu List
+# Create an off-screen buffer and drawing object for Generation VI Menu
 buffer_generation_vi_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_vi_menu = ImageDraw.Draw(buffer_generation_vi_menu)
-# Create an off-screen buffer and drawing object for Generation VII Menu List
+# Create an off-screen buffer and drawing object for Generation VII Menu
 buffer_generation_vii_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_vii_menu = ImageDraw.Draw(buffer_generation_vii_menu)
-# Create an off-screen buffer and drawing object for Generation VIII Menu List
+# Create an off-screen buffer and drawing object for Generation VIII Menu
 buffer_generation_viii_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_viii_menu = ImageDraw.Draw(buffer_generation_viii_menu)
-# Create an off-screen buffer and drawing object for Generation IX Menu List
+# Create an off-screen buffer and drawing object for Generation IX Menu
 buffer_generation_ix_menu = Image.new("1", (disp.width, disp.height))
 draw_generation_ix_menu = ImageDraw.Draw(buffer_generation_ix_menu)
+# Create an off-screen buffer and drawing object for Generation I Main Region Menu
+buffer_generation_i_main_region_menu = Image.new("1", (disp.width, disp.height))
+draw_generation_i_main_region_menu = ImageDraw.Draw(buffer_generation_i_main_region_menu)
 
 # Define states
 MAIN_MENU_STATE = 0
@@ -77,6 +80,8 @@ GENERATION_VI_MENU_STATE = 7
 GENERATION_VII_MENU_STATE = 8
 GENERATION_VIII_MENU_STATE = 9
 GENERATION_IX_MENU_STATE = 10
+
+GENERATION_I_MAIN_REGION_MENU_STATE = 11
 
 # Initialize the current state and the selected menu item
 current_state = MAIN_MENU_STATE
@@ -93,6 +98,8 @@ start_index_generation_vi_menu = 0
 start_index_generation_vii_menu = 0
 start_index_generation_viii_menu = 0
 start_index_generation_ix_menu = 0
+
+start_index_generation_i_main_region_menu = 0
 
 def clear_buffer(buffer, draw):
     draw.rectangle((0, 0, buffer.width, buffer.height), outline=0, fill=0)
@@ -120,6 +127,17 @@ def fetch_generation_data():
         if response.status_code == 200:
             generation_data = response.json().get("results", [])
             return generation_data
+        else:
+            print(f"Failed to get Generations data. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+
+def fetch_generation_i_data():
+    try:
+        response = requests.get(generation_api_url + "/1/")
+        if response.status_code == 200:
+            generation_i_data = response.json()
+            return generation_i_data
         else:
             print(f"Failed to get Generations data. Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
@@ -307,6 +325,21 @@ def update_display_generation_ix_menu(selected_index_generation_ix_menu):
 
     disp.image(buffer_generation_ix_menu)
     disp.show()
+
+def update_display_generation_i_main_region_menu(selected_index_generation_i_main_region_menu):
+    clear_buffer(buffer_generation_i_main_region_menu, draw_generation_i_main_region_menu)
+    draw_generation_i_main_region_menu.text((0, 0), "Main Regions:", fill=1)
+
+    display_count = 5
+    generation_i_data = fetch_generation_i_data()
+    main_region_data = [generation_i_data["main_region"]]
+    total_generation_i_main_region_menu_items = len(main_region_data)
+    start_index_generation_i_main_region_menu = 0
+    max_visible_items = min(display_count, total_generation_i_main_region_menu_items - start_index_generation_i_main_region_menu)
+
+    
+
+
 
 while True:
 
@@ -595,3 +628,5 @@ while True:
                 print("Button B Pressed")
                 current_state = GENERATIONS_MENU_STATE
                 break
+
+    
